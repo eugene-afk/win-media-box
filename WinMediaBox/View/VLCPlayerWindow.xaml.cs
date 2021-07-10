d7406814e644b36c396c1045c9b634208e6eb7d0
eugene-afk <cheackybreacky@gmail.com>
2021-07-10 22:24:09 +0300

    vlc iptv input logic

--- WinMediaBox/View/VLCPlayerWindow.xaml.cs
+++ WinMediaBox/View/VLCPlayerWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using LibVLCSharp.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WinMediaBox.Classes;
 using WinMediaBox.ViewModel;
 
@@ -25,21 +15,19 @@ namespace WinMediaBox.View
         {
             InitializeComponent();
             _vm = new VLCPlayerViewModel(playlist);
+            DataContext = _vm;
             videoView.Loaded += VideoView_Loaded;
         }
 
         void VideoView_Loaded(object sender, RoutedEventArgs e)
         {
-            videoView.MediaPlayer = _vm._vlcService.player;
-            _vm._vlcService.player.Play();
+            videoView.MediaPlayer = _vm.vlcService.player;
+            _vm.vlcService.player.Play();
         }
 
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key != Key.Enter)
-            {
-
-            }
+            _vm.InputAction(e.Key);
         }
 
         private void Window_Closed(object sender, EventArgs e)
--- WinMediaBox/ViewModel/VLCPlayerViewModel.cs
+++ WinMediaBox/ViewModel/VLCPlayerViewModel.cs
@@ -1,35 +1,147 @@
 ﻿using LibVLCSharp.Shared;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WinMediaBox.Classes;
 
 namespace WinMediaBox.ViewModel
 {
-    public class VLCPlayerViewModel : IDisposable
+    public class VLCPlayerViewModel : Base, IDisposable
     {
-        public VLCService _vlcService;
+        public VLCService vlcService;
         private M3U8Playlist _playlist;
+
+        private string _channelName { get; set; } = "UNKNOWN";
+        public string channelName
+        {
+            get { return _channelName; }
+            set { _channelName = value; OnPropertyChanged(); }
+        }
+
+        private Visibility _infoBlockVisible { get; set; } = Visibility.Collapsed;
+        public Visibility infoBlockVisible
+        {
+            get { return _infoBlockVisible; }
+            set { _infoBlockVisible = value; OnPropertyChanged(); }
+        }
+
+        private int _channelNumber { get; set; } = 0;
+        public int channelNumber
+        {
+            get { return _channelNumber; }
+            set { _channelNumber = value; OnPropertyChanged(); }
+        }
+
+        private M3U8Item _currentChannel;
+        private int _channelsCount;
+        public bool isInfoBlockVisible => infoBlockVisible != Visibility.Collapsed;
 
         public VLCPlayerViewModel(M3U8Playlist playlist)
         {
             _playlist = playlist;
-            _vlcService = new VLCService();
+            vlcService = new VLCService();
+            _channelsCount = _playlist.Count();
             SetupPlayer();
         }
 
         public void SetupPlayer()
         {
-            Media media = new Media(_vlcService.libVLC, new Uri(_playlist[0].url));
-            _vlcService.player = new MediaPlayer(media);
+            _currentChannel = _playlist[0];
+            channelName = _currentChannel.title;
+            channelNumber = _currentChannel.number;
+            Media media = new Media(vlcService.libVLC, new Uri(_currentChannel.url));
+            vlcService.player = new MediaPlayer(media);
             media.Dispose();
+        }
+
+        private void Next()
+        {
+            int num = _currentChannel.number;
+            if(num >= _channelsCount)
+            {
+                num = 0;
+            }
+            SetNewMedia(num);
+        }
+
+        private void Prev()
+        {
+            int num = _currentChannel.number - 2;
+            if (num <= 0)
+            {
+                num = _channelsCount - 1;
+            }
+            SetNewMedia(num);
+        }
+
+        private async Task ShowInfoBlock(bool force = false)
+        {
+            if (!isInfoBlockVisible)
+            {
+                infoBlockVisible = Visibility.Visible;
+                await Task.Delay(3000);
+                infoBlockVisible = Visibility.Collapsed;
+                if (force)
+                {
+                    if (channelNumber >= 0 && channelNumber <= _channelsCount && channelNumber != _currentChannel.number)
+                    {
+                        SetNewMedia(channelNumber - 1);
+                        return;
+                    }
+                    channelNumber = _currentChannel.number;
+                }
+            }
+        }
+
+        public void InputAction(Key key)
+        {
+            if (key == Key.Enter)
+            {
+                Task.Run(() => ShowInfoBlock());
+                return;
+            }
+
+            if (key >= Key.D0 && key <= Key.D9)
+            {
+                if (!isInfoBlockVisible)
+                {
+                    channelNumber = 0;
+                    Task.Run(() => ShowInfoBlock(true));
+                }
+                string num = channelNumber != 0 ? channelNumber.ToString() : "";
+                var value = $"{num}{key.ToString().Replace("D", "")}";
+
+                channelNumber = Convert.ToInt32(value);
+                return;
+            }
+
+            if (key == Key.PageUp)
+            {
+                Next();
+                return;
+            }
+
+            if (key == Key.PageDown)
+            {
+                Prev();
+            }
+        }
+
+        private void SetNewMedia(int num)
+        {
+            _currentChannel = _playlist[num];
+            channelName = _currentChannel.title;
+            channelNumber = _currentChannel.number;
+            vlcService.player.Media = new Media(vlcService.libVLC, new Uri(_currentChannel.url));
+            vlcService.player.Play();
+            Task.Run(() => ShowInfoBlock());
         }
 
         public void Dispose()
         {
-            _vlcService.Dispose();
+            vlcService.Dispose();
         }
     }
 }
